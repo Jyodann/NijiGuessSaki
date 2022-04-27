6cf95d9329340412aff29cfb93db6592c48fec08
Jordan <jordynwinnie@gmail.com>
2022-04-27 23:58:23 +0800

    Added IHttpClientFactory

--- Program.cs
+++ Program.cs
@@ -4,8 +4,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
-builder.Services.AddScoped(sp =>
-    new HttpClient());
+builder.Services.AddHttpClient();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.

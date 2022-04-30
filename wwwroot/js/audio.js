51dfd62a4d7b53683df143338fb0380c01a9575f
Jordan Khong <jordynwinnie@gmail.com>
2022-04-30 21:10:59 +0800

    iOS Hotfix

--- wwwroot/js/audio.js
+++ wwwroot/js/audio.js
@@ -1,23 +1,30 @@
-const soundEffect = new Audio()
+const soundEffect = new Audio("https://p.scdn.co/mp3-preview/3d26cfb135fb88e9dd2fa7cb7874f69af6fef4dd?cid=4731e5a17e30449cb5d298a1c886200c")
 
-function loadAudio(element, audioSource) {
-    console.log(audioSource);
-    element.src = audioSource;
-    element.load()
-}
-function playAudio(element, startDuration) {
-    element.currentTime = startDuration;
-    element.play();
-}
-function stopAudio(element) {
-    element.pause();
-    element.currentTime = 0;
+
+function iosUnlock() {
+    document.getElementById("audioPlayer").play()
+    document.getElementById("audioPlayer").pause()
+
+    document.getElementById("iosUnlock").disabled = true;
 }
 
-function setVolume(element, volume) {
-    element.volume = volume;
+function loadAudio(audioSource) {
+    document.getElementById("audioPlayer").src = audioSource
+    document.getElementById("audioPlayer").load()
+}
+function playAudio(startDuration) {
+    document.getElementById("audioPlayer").currentTime = startDuration;
+    document.getElementById("audioPlayer").play();
+}
+function stopAudio() {
+    document.getElementById("audioPlayer").pause();
+    document.getElementById("audioPlayer").currentTime = 0;
 }
 
-function returnDuration(element) {
-    return element.currentTime.toString()
+function setVolume(volume) {
+    document.getElementById("audioPlayer").volume = volume;
+}
+
+function returnDuration() {
+    return document.getElementById("audioPlayer").currentTime.toString()
 }

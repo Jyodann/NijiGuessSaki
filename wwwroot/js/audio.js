4324b654b0162ea410cf3fc89a95bb9067999678
Jordan <jordynwinnie@gmail.com>
2022-04-30 22:31:15 +0800

    Removing iOS Unlock Button

--- wwwroot/js/audio.js
+++ wwwroot/js/audio.js
@@ -1,30 +1,34 @@
-const soundEffect = new Audio("https://p.scdn.co/mp3-preview/3d26cfb135fb88e9dd2fa7cb7874f69af6fef4dd?cid=4731e5a17e30449cb5d298a1c886200c")
+const audioPlayer = null;
+
+window.onload = (event) => {
+    audioPlayer = document.getElementById("audioPlayer")
+}
 
 
 function iosUnlock() {
-    document.getElementById("audioPlayer").play()
-    document.getElementById("audioPlayer").pause()
+    audioPlayer.play()
+    audioPlayer.pause()
 
     document.getElementById("iosUnlock").disabled = true;
 }
 
 function loadAudio(audioSource) {
-    document.getElementById("audioPlayer").src = audioSource
-    document.getElementById("audioPlayer").load()
+    audioPlayer.src = audioSource
+    audioPlayer.load()
 }
 function playAudio(startDuration) {
-    document.getElementById("audioPlayer").currentTime = startDuration;
-    document.getElementById("audioPlayer").play();
+    audioPlayer.currentTime = startDuration;
+    audioPlayer.play();
 }
 function stopAudio() {
-    document.getElementById("audioPlayer").pause();
-    document.getElementById("audioPlayer").currentTime = 0;
+    audioPlayer.pause();
+    audioPlayer.currentTime = 0;
 }
 
 function setVolume(volume) {
-    document.getElementById("audioPlayer").volume = volume;
+    audioPlayer.volume = volume;
 }
 
 function returnDuration() {
-    return document.getElementById("audioPlayer").currentTime.toString()
+    return audioPlayer.currentTime.toString()
 }
